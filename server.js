0fdb350f67afb8928b45c8a17661d70eb53966ad
Sudhanshu Burile <123616951+sudhanshu-b@users.noreply.github.com>
2024-09-18 15:35:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json({ limit: '200mb' }));
 app.use(express.urlencoded({ limit: '200mb', extended: true }));
 
 //MongoDB Connection and Schema defination
-mongoose.connect('mongodb+srv://sudhaanshu04:kI6Ti0Fk6vKjn72e@labellingui.eprcq.mongodb.net/?retryWrites=true&w=majority&appName=labellingUI');
+mongoose.connect('mongodb+srv://sudhaanshu04:kI6Ti0Fk6vKjn72e@labellingui.eprcq.mongodb.net/?retryWrites=true&w=majority&appName=labellingUI/Labelling-pro');
 
 const licenseSchema = new mongoose.Schema({
     name: String,
